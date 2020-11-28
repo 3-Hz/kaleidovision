457bb3e5efbc0615de074c6e721c6a445ccbbe34
3-Hz <edward.h.zhao@gmail.com>
2020-11-27 20:31:54 -0600

    working 4 tile kaleidoscope with axial symmetry

--- src/sketch.js
+++ src/sketch.js
@@ -17,7 +17,7 @@ function setup() {
 // main loop
 function draw() {
   var tile = new Tile(sourceImage, tileWidth, tileHeight);
-  var grid = new Grid(5, 5);
+  var grid = new Grid(2, 2);
   grid.update(tile);
 };
 
@@ -29,24 +29,32 @@ class Grid {
   }
 
   update(tile) {
-    // for (let i = 0; i < this.width; i++) {
-    //   for (let j = 0; j < this.height; j++) {
-    //     if (i % 2 !== 0 && j % 2 !== 0) {
-    //       // 180 degree rotate lower right corner
-    //     } else if (i % 2 === 0 && j % 2 !== 0) {
-    //       // 270 degree rotate lower left corner
-    //     } else if (i % 2 !== 0 && j % 2 === 0) {
-    //       // 90 degree rotate upper right corner
-    //     } else if (i % 2 === 0 && j % 2 === 0) {
-    //       // 0 or 360 degree rotate upper left corner
-    //       image(tile.image, i * tile.image.width, j * tile.image.height);
-    //     }
-    //   }
-    // }
-    image(tile.image, 0, 0, tile.width/2, tile.height); //video on canvas, position, dimensions
-    translate(tile.width,0); // move to far corner
-    scale(-1.0,1.0);    // flip x-axis backwards
-    image(tile.image, 0, 0, tile.width/2, tile.height); //video on canvas, position, dimensions
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        // if (i % 2 !== 0 && j % 2 !== 0) {
+        //   // 180 degree rotate lower right corner
+        // } else if (i % 2 === 0 && j % 2 !== 0) {
+        //   // 270 degree rotate lower left corner
+        // } else if (i % 2 !== 0 && j % 2 === 0) {
+        //   // 90 degree rotate upper right corner
+        // } else if (i % 2 === 0 && j % 2 === 0) {
+        //   // 0 or 360 degree rotate upper left corner
+        //   image(tile.image, i * tile.image.width, j * tile.image.height);
+        // }
+        image(tile.image, 0, 0, tile.width/2, tile.height/2);
+        translate(tile.width * (i + 1),0);
+        scale(-1.0,1.0);
+        image(tile.image, 0, 0, tile.width/2, tile.height/2); //video on canvas, position, dimensions
+        translate(0, tile.height * (j + 1));
+        scale(1.0,-1.0);
+        image(tile.image, 0, 0, tile.width/2, tile.height/2);
+        translate(tile.width * (i + 1), 0);
+        scale(-1.0, 1.0)
+        image(tile.image, 0, 0, tile.width/2, tile.height/2);
+      }
+    }
+
+
   }
 }
 
